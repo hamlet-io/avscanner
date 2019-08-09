77222a9d2b00c8fe6093805160461e3cf405bc0f
Neketek <neketek@gmail.com>
2019-08-09 21:07:59 +0300

    Removed shutdown functionality from queue polling worker base class

--- src/processor/common/worker/queue_polling.py
+++ src/processor/common/worker/queue_polling.py
@@ -1,4 +1,3 @@
-import time
 from common import loggers
 
 
@@ -9,30 +8,15 @@ class QueuePollingWorker:
 
     MESSAGE_WAIT_TIME = 0
     MESSAGE_VISIBILITY_TIMEOUT = 0
-    SHUTDOWN_CHECK_TIME_INTERVAL = 60
-    SHUTDOWN_CHECK_MIN_MESSAGES = 1
 
     def __init__(self, queue_dao=None, logger=None):
         self.__queue = queue_dao
         self.logger = logger if logger else queue_polling_worker_logger
-        self.__shutdown_check_time = 0
-        self.__shutdown_check_messages_received = 0
-        self.__stop = False
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.__stop:
-            self.logger.info(
-                'Shutdown. Messages %i/%i in %i seconds.',
-                self.__shutdown_check_messages_received,
-                self.SHUTDOWN_CHECK_MIN_MESSAGES,
-                self.SHUTDOWN_CHECK_TIME_INTERVAL
-            )
-            raise StopIteration()
-
-        started_time = time.time()
 
         message = self.__queue.get(
             wait_time=self.MESSAGE_WAIT_TIME,
@@ -41,19 +25,11 @@ class QueuePollingWorker:
 
         if message is not None:
             self.logger.info('Message received.')
-            self.__shutdown_check_messages_received += 1
             if self.process_message(message):
                 self.logger.info('Message processed succesfully, deleting message.')
                 self.__queue.delete(message=message)
             else:
                 self.logger.info('Message processing failed.')
-
-        self.__shutdown_check_time += time.time() - started_time
-        if self.__shutdown_check_time >= self.SHUTDOWN_CHECK_TIME_INTERVAL:
-            if self.__shutdown_check_messages_received < self.SHUTDOWN_CHECK_MIN_MESSAGES:
-                self.__stop = True
-            else:
-                self.__shutdown_check_messages_received = 0
 
         return message is not None
 
--- src/tests/integration/common/worker/test_queue_polling.py
+++ src/tests/integration/common/worker/test_queue_polling.py
@@ -32,25 +32,3 @@ def test():
     worker.process_message = successful_processing
     assert next(worker)
     assert not queue_dao.get(wait_time=0, visibility_timeout=0)
-
-
-@pytest.mark.usefixtures(
-    'clear_queues'
-)
-def test_shutdown():
-    queue_dao = queue.VirusScanning(conf.get_sqs_env_conf())
-    worker = QueuePollingWorker(queue_dao=queue_dao)
-
-    worker.SHUTDOWN_CHECK_MIN_MESSAGES = 3
-    worker.SHUTDOWN_CHECK_TIME_INTERVAL = 4
-    worker.MESSAGE_WAIT_TIME = 1
-    worker.MESSAGE_VISIBILITY_TIMEOUT = 60
-    worker.process_message = lambda m: True
-
-    counter = 0
-    messages = 5
-    for i in range(messages):
-        queue_dao.post(body=str(i), delay=i+1)
-    for result in worker:
-        counter += 1 if result else 0
-    assert counter == messages
