fe4bddca99f0ca6e05ffa13304e1e63b7fcd56c0
Kseniya Shychko <k.shychko@gmail.com>
2019-09-01 03:08:50 +0300

    creating virus notification SNS topic using awscli

--- src/docker/scripts/entrypoint-ci.sh
+++ src/docker/scripts/entrypoint-ci.sh
@@ -4,4 +4,14 @@
 echo "Starting clamd..."
 clamd
 echo "Clamd started"
+
+echo "Installing awscli using pip"
+pip3 install awscli --upgrade
+aws --version
+
+echo "Creating SNS virus notification topic"
+AWS_ACCESS_KEY_ID=${AWS_SNS_ACCESS_KEY_ID} \
+AWS_SECRET_ACCESS_KEY=${AWS_SNS_SECRET_ACCESS_KEY} \
+aws --region=${AWS_SNS_REGION} --endpoint-url=${AWS_SNS_ENDPOINT_URL} sns create-topic --name ${VIRUS_NOTIFICATION_TOPIC_ARN##*:}
+
 make test-ci
