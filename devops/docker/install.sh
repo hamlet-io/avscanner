ad5a80df482db1148ccc8066e2a1a0f446c22339
Michael Foley <roleyfoley@gmail.com>
2020-01-31 12:37:54 +1100

    <fix> ensure that AV is always updated on startup (#47)

--- devops/docker/install.sh
+++
@@ -1,7 +0,0 @@
-apk add make
-apk add zip
-
-pip install --upgrade pip
-
-# to avoid various permission problems
-chmod -R 777 /tmp
--- src/docker/scripts/entrypoint-avscanner.sh
+++ src/docker/scripts/entrypoint-avscanner.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Always refresh av on startup
+freshclam -v -u clamav
 clamd
 python processor/worker/virus_scanner/__init__.py
